7d7e40a8499089afcd4ae6a70d8dcea9e61a2ac1
ROHIT LODHA RTG <Rohit Lodha>
2018-02-24 04:03:39 +0530

    first and follow version 1

--- parser.c
+++ parser.c
@@ -3,28 +3,32 @@
 #include <ctype.h>
 #include <string.h>
 
-ttypedef struct grammarnode{
-	int type;  //type =1 for non terminal, type=0 for terminal, type=2 for or1, type=3 for epsilon
-	int value;
-	char name[40];
-	int epsilon;  // epsilon =1 for epsilon, epsilon=0 for others
-	struct grammernode *next;
-	struct grammernode *prev;
-} grammarnode;
+// typedef struct grammarnode{
+	// int type;  //type =1 for non terminal, type=0 for terminal, type=2 for or1, type=3 for epsilon
+	// int value;
+	// char name[40];
+	// struct grammernode *next;
+	// struct grammernode *prev;
+// } grammarnode;
 
-typedef grammarnode *GrammarNode;
+// typedef grammarnode *GrammarNode;
 
-typedef struct rulenode{
-	int lineno;
-	GrammarNode head;
-	GrammarNode tail;
-} Rule;
+// typedef struct rulenode{
+	// int lineno;
+	// GrammarNode head;
+	// GrammarNode tail;
+// } Rule;
 
-Rule* grammer[NO_OF_RULES];
+// Rule* grammer[NO_OF_RULES];
+
+int NO_OF_TERMINALS=0;
+int NO_OF_NONTERMINALS=1;
 
 GrammarNode makenode(int type,int value,char* name){
 	GrammarNode g = (GrammarNode)malloc(sizeof(struct grammarnode));
 	g->type = type;
+	if(type==0) NO_OF_TERMINALS++;
+	else if(type==1) NO_OF_NONTERMINALS++;
 	g->value = value;
 	strcpy(g->name,name);
 	// if(type==1){
@@ -112,7 +116,7 @@ void printGrammar(){
 		Rule* rule = grammer[i];
 		GrammarNode temp = rule->head;
 		while(temp!=NULL){
-			printf("%d-%s ",temp->type,temp->name);
+			printf("%s ",temp->name);
 			temp=temp->next;
 		}
 		printf("\n");
@@ -124,3 +128,71 @@ void printGrammar(){
 	// printf("Done");
 	// printGrammar();
 // }
+
+int** firstMatrix = (int **)malloc(sizeof(int*)*NO_OF_NONTERMINALS);
+int** followMatrix = (int **)malloc(sizeof(int*)*NO_OF_NONTERMINALS);
+
+void initialisefirstandfollowMatrix(){
+	int i,j;
+	for(i=0;i<NO_OF_TERMINALS;i++){
+		firstMatrix[i] = (int*)malloc(sizeof(int)*NO_OF_TERMINALS);
+		followMatrix[i] = (int*)malloc(sizeof(int)*NO_OF_TERMINALS);
+		for(j=0;j<NO_OF_TERMINALS;j++){
+			firstMatrix[i][j]=0;
+			followMatrix[i][j]=0;
+		}
+	}
+}
+
+void findFirst(int value,int parent,int* eps){
+	// for (i=0;i<NO_OF_TERMINALS;i++){
+		// if
+	// }
+	// if already found in hash
+	// can keep local int* and send it to add as apar
+	int i,j;
+	for (i=0;i<NO_OF_RULES;i++){
+		if (strcmp(grammer[i]->head->value,value){
+			// if hashed present
+			if (grammer[i]->head->next->type==3){ //epsilon
+				*eps = 1;
+				findFirst[value][NO_OF_TERMINALS-1]=1;
+				findFirst[parent][NO_OF_TERMINALS-1]=1;
+				// check for follow
+				// add(parent,epsilon);
+			}
+			else{
+				GrammarNode temp = grammer[i]->head->next;
+				while (temp!=NULL){
+					eps = 0; //epsilon
+					int eps1;
+					findFirst(hash(temp->name),parent,&eps1)
+					if(eps1!=1) break;
+					else temp=temp->next;
+				}
+			}
+		}
+	}
+}
+
+int count=0;
+void findfollow(int value,int parent,int* eps){
+	// if hashed
+	// if main add $ (eps)
+	for (i=0;i<NO_OF_RULES;i++){
+		GrammarNode temp = grammer[i]->head->next;
+		while(temp!=NULL){
+			if(temp->value==value){
+				if(temp->next->value==NULL) {// don't check for A->A recursion
+					int eps;
+					findfollow(hash(grammer[i]->head),parent,&eps);
+				}
+				else {
+					findfirst(hash(temp->next),parent,&eps);
+				}
+			}
+		}
+	}
+}
+
+
--- parserDef.h
+++ parserDef.h
@@ -5,23 +5,20 @@
 
 // can also use enum in plase of int type
 typedef struct grammarnode{
-	int type;  //type =1 for non terminal, type=0 for terminal
+	int type;  //type =1 for non terminal, type=0 for terminal, type=2 for |, type=3 for epsilon
 	int value;
-	int or1;
 	char name[40];
-	int epsilon;  // epsilon =1 for epsilon, epsilon=0 for others
 	struct grammernode *next;
 	struct grammernode *prev;
-}
+} grammarnode;
 
-typedef *grammarnode GrammarNode;
+typedef grammarnode *GrammarNode;
 
-typdef struct rulenode{
+typedef struct rulenode{
 	int lineno;
 	GrammarNode head;
 	GrammarNode tail;
 } Rule;
 
 Rule* grammer[NO_OF_RULES];
-
 #endif PARSERDEFINATIONHEADER
